d331a4d7dfe368dedb44e115a624d250034458c4
aminul157246 <aminul.islam.157246@gmail.com>
2025-04-14 11:32:17 +0600

    project set up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='max-w-7xl mx-auto'>
+      <h2 className='text-5xl text-white'>Hello there!</h2>
+
+    </div>
+  );
+};
+
+export default App;
